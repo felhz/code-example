1137a968a97c9535dcf10543337c26a03157783c
lihongzhao <hongzhao.li@eeoa.com>
2024-07-09 14:32:07 +0800

    feat: 添加正则

--- js/正则.js
+++ js/正则.js
@@ -17,3 +17,17 @@ while ((match = regex.exec(str)) !== null) {
 }
 // 输出 Found quick brown at index 4
 // 输出 Group 1: brown
+
+// 请输入6-20位密码，包含数字字母
+export const PWD_REG = /^(?=.*\d)(?=.*[a-zA-Z]).{6,20}$/;
+
+// 空格
+// export const SPACE_REG = /(^\s*)|(\s*$)/g
+export const SPACE_REG = /\s+/g;
+
+//中文
+export const CHINESE_REG = /[\u4e00-\u9fa5]]/gi;
+
+export const mobileRegex = /^1[3456789]\d{9}$/;
+export const emailRegex =
+  /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$/;
