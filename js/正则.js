70373f1d7cd9e2d6c2f0479f66728155bbc13b1b
lihongzhao <hongzhao.li@eeoa.com>
2024-07-09 11:47:37 +0800

    feat: 更新文件

--- js/正则.js
+++ js/正则.js
@@ -6,3 +6,14 @@ let str = 'aa12dawdwa';
 str.replace(reg, (...args) => {
   console.log(args);
 });
+
+const regex = /quick (\w+)/g;
+const str = 'The quick brown fox jumps over the lazy dog.';
+let match;
+
+while ((match = regex.exec(str)) !== null) {
+  console.log(`Found ${match[0]} at index ${match.index}`);
+  console.log(`Group 1: ${match[1]}`);
+}
+// 输出 Found quick brown at index 4
+// 输出 Group 1: brown
